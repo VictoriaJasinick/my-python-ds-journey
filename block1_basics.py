7b4ef7f369c227c6d702a746b99f14a20886c751
VictoriaJasinick <kolibriland@gmail.com>
2025-12-08 08:26:31 +0100

    basic Python concepts (variables, input, operations)

---
+++ block1_basics.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+my_name="Victoria"
+print("Привет, я - " + my_name + "!")    
+price = 5.99
+qty = 3
+print(price + qty)
+print(price - qty)
+print(price / qty)
+print(price // qty)
+print(price % qty)
+text = "Привет, мир!"
+print(text.upper())
+print(text.upper())
+print(text.lower())
+print(len(text))
+print(text[-1])
+
+name=input("Как тебя зовут? ")
+age=int(input("Сколько тебе лет? "))
+print("Привет, " + name + "!" + "Через год тебе будет " + str(age+1))
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
