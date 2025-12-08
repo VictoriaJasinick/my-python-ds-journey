3d45cfe6fef2be488fa95245bc92e02c7dfb4628
VictoriaJasinick <kolibriland@gmail.com>
2025-12-08 08:45:05 +0100

    Translate comments and update variable examples

---
+++ 01_python_basics_variables_input.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+# Example of a variable with a name
+my_name = "Victoria"
+print("Hello, I am " + my_name + "!")
+
+# Example of working with numbers
+number1 = 5.99  # First number (could be any float)
+number2 = 3     # Second number (could be any integer)
+
+# Arithmetic operations using two numbers
+print(number1 + number2)   # Addition
+print(number1 - number2)   # Subtraction
+print(number1 * number2)   # Multiplication
+print(number1 / number2)   # Division (result includes decimals)
+print(number1 // number2)  # Floor division (integer result, no decimals)
+print(number1 % number2)   # Modulus (remainder after division)
+
+# String operations
+text = "Hello, world!"
+print(text.upper())  # Convert to UPPERCASE
+print(text.lower())  # Convert to lowercase
+print(len(text))     # Get the length of the string
+print(text[-1])      # Access the last character of the string
+
+# User input and working with data types
+name = input("What's your name? ")
+age = int(input("How old are you? "))  # Convert the input age to an integer
+print("Hello, " + name + "! Next year you'll be " + str(age + 1))
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
